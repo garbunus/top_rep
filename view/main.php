cf7c71afdb53134c825b772ce9ab0d2df912fc70
Eduardo Garbin <garbunus@gmail.com>
2017-11-30 14:24:14 -0400

    Estrutura view principal

---
+++ view/main.php
@@ -0,0 +1,25 @@
+<?php ?>
+<!DOCTYPE html>
+<HTML>
+  <HEAD>
+    <meta charset="UTF-8">
+    <title>top_repo</title>
+
+    <!-- ADD JQUERY -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+    <!-- ADD SCRIPTS DESENVOLVEDOR-->
+    <script src="../JS/JS_relacionamento_git.js" type="text/javascript"></script>
+    <script src="../JS/JS_repositorios.js" type="text/javascript"></script>
+
+    <!-- ADD STYLE-->
+    <link href="../estilo/estilo.css" rel="stylesheet" type="text/css"/>
+    <link rel="shortcut icon" href="/img/logo.png" type="image/x-icon" />
+
+  </HEAD>
+  <BODY>
+    <div id="app"><?php ?></div>
+  </BODY>
+</HTML>
