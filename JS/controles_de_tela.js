5fc7817d7d09ba41adabe07309a675aeb8dad430
Eduardo Garbin <garbunus@gmail.com>
2017-11-30 18:53:03 -0400

    update controle de telas

---
+++ JS/controles_de_tela.js
@@ -0,0 +1,18 @@
+/*###################################################################################
+              Controle de exibição de telas de busca de repositório
+                    e visualização de repositórios armazenado
+###################################################################################*/
+
+function mostra_tela_busca(){
+   $('.telas').css('display', 'none');
+   $('#tela_busca').css('display','block');
+}
+
+function mostra_tela_armazenados(){
+  $('.telas').css('display', 'none');
+  $('#tela_armazenados').css('display','block');
+}
+
+$(document).ready(function () {
+  mostra_tela_busca();
+});
