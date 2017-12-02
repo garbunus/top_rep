1928907b302b78dcec18617c1ce46a19db9bbf2b
Eduardo Garbin <garbunus@gmail.com>
2017-12-02 15:38:16 -0400

    retorno para funções

--- model/repositorio.php
+++ model/repositorio.php
@@ -1,31 +1,35 @@
 <?php
 
-require_once("/DAO/DAO_repositorio.php");
+require_once("DAO/DAO_repositorio.php");
 
 class repositorios{
 
   //########################### Grava dados no banco ###########################
-    public function grava_repositorios($id, $nome, $linguagem, $descricao, $html_url, $url, $criado_em, $forks){
-      $dao = new DAO_repositorios;
-      $dao->DAO_grava_repositorios($id, $nome, $linguagem, $descricao, $html_url, $url, $criado_em, $forks);
+    public function grava_repositorio($id, $nome, $linguagem, $descricao, $html_url, $url, $criado_em, $forks){
+      $dao = new DAO_repositorio;
+      $retorno = $dao->DAO_grava_repositorio($id, $nome, $linguagem, $descricao, $html_url, $url, $criado_em, $forks);
+      return $retorno;
     }
 
   //########################### Busca dados no banco ###########################
     public function busca_repositorios(){
-      $dao = new DAO_repositorios;
-      $dao->DAO_busca_repositorios();
+      $dao = new DAO_repositorio;
+      $retorno = $dao->DAO_busca_repositorios();
+      return $retorno;
     }
 
-  //########################### Busca dados no banco ###########################
-    public function busca_detalhes_repositorios($id){
-      $dao = new DAO_repositorios;
-      return $dao->DAO_busca_detalhes_repositorios($id);
+  //######### Busca detalhes de repositório selecionado dados no banco #########
+    public function busca_detalhes_repositorio($id){
+      $dao = new DAO_repositorio;
+      $retorno = $dao->DAO_busca_detalhes_repositorio($id);
+      return $retorno;
     }
 
-  //########################### Apaga dados no banco ###########################
-    public function apaga_repositorios($id){
-      $dao = new DAO_repositorios;
-      return $dao->DAO_apaga_repositorios($id);
+  //################## Apaga repositório selecionado do banco ##################
+    public function apaga_repositorio($id){
+      $dao = new DAO_repositorio;
+      $retorno = $dao->DAO_apaga_repositorio($id);
+      return $retorno;
     }
 
 
