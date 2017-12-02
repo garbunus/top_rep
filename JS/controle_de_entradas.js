ddf8f31d44ed88317c7d6a7300c1c68af5d0e2f7
Eduardo Garbin <garbunus@gmail.com>
2017-12-02 09:33:51 -0400

    add função para verificação de valor nulo

--- JS/controle_de_entradas.js
+++ JS/controle_de_entradas.js
@@ -8,3 +8,10 @@ function destacar(item){
     $('.linha_lista').removeClass('linha_lista_checked');
     $(item).addClass('linha_lista_checked');
 }
+
+//verifica se valor é nulo
+function se_nulo(valor){
+  if valor == null{
+    return "Não há informação disponível."
+  }
+}
