08675ce87d5ccb1458d225205713f63c055935b0
Eduardo Garbin <garbunus@gmail.com>
2017-12-02 15:16:57 -0400

    add telas de feedback e detalhes

---
+++ view/feedback.php
@@ -0,0 +1,4 @@
+<!-- tela de loading e alertas -->
+<div class="fundo_feedback" id="check_ok"><div><div id="img_ok"><img src="img/ok.png"/></div><div><h3 id="feedback_text_ok"></h3></div></div></div>
+<div class="fundo_feedback" id="check_erro"><div><div id="img_erro"><img src="img/erro.png"/></div><div><h3 id="feedback_text_erro"></h3></div></div></div>
+<div class="fundo_feedback" id="loading"><div><img src="img/loader.gif"/><h3 style="color:#333333; text-align:center;">Aguarde...</h3></div></div>
