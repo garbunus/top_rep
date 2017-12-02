ac5534ef6f003ebfa17ffe5dd9e8b9f3773e7169
Eduardo Garbin <garbunus@gmail.com>
2017-12-02 10:07:20 -0400

    update controle de funções

--- JS/controle_de_funcoes.js
+++ JS/controle_de_funcoes.js
@@ -3,21 +3,21 @@ EXECUTA ALTERAÇÕES SOBRE DIVERSAS FUNÇÕES COMO ALTERAÇÃO DE ARGUMENTOS E C
 #####################################################################################*/
 
 //altera argumento da função "grava_repositorio()", setando qual repositorio deve ser gravado
-function escreve_arg_grava_repositorio(ident){
-  $("#bt_grava_repositorio").attr("onclick","grava_repositorio("+ ident +")");
+function escreve_arg_grava_repositorio(id, commits, contributors, name, language, description, html_url, url, created_at, forks){
+  $('#bt_grava_repositorio').attr('onclick','grava_repositorio(' + id + ', ' + commits + ', ' + contributors + ', \'' + name + '\',\'' + language + '\', \'' + description + '\', \'' + html_url + '\', \'' + url + '\', \'' + created_at + '\', ' + forks + ')');
 }
 
 //altera argumento da função "detalhe_repositorio()", setando qual repositorio deve ser visualizado
-function escreve_arg_grava_repositorio(ident){
-  $("#bt_detalhe_repositorio").attr("onclick","detalhe_repositorio("+ ident +")");
+function escreve_arg_detalhe_repositorio(id, commits, contributors, name, language, description, html_url, url, created_at, forks){
+  $('#bt_detalhe_repositorio').attr('onclick','detalhe_repositorio('+ id + ',' + commits + ',' + contributors + ', \'' + name + '\', \'' + language + '\', \'' + description + '\', \'' + url + '\', \'' + created_at + '\', ' + forks + ')');
 }
 
 //altera argumento da função "apaga_repositorio_armazenado()", setando qual repositorio deve ser apagado
-function escreve_arg_grava_repositorio(ident){
+function escreve_arg_apaga_repositorio_armazenado(ident){
   $("#bt_apaga_repositorio_armazenado").attr("onclick","apaga_repositorio_armazenado("+ ident +")");
 }
 
 //altera argumento da função "detalhe_repositorio()", setando qual repositorio deve ser visualizado
-function escreve_arg_grava_repositorio(ident){
+function escreve_arg_detalhe_repositorio_armazenado(ident){
   $("#bt_detalhe_repositorio_armazenado").attr("onclick","detalhe_repositorio_armazenado("+ ident +")");
 }
