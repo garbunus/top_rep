45ac6e50f0fed94c77a8fb2819313186b7a18d03
Eduardo Garbin <garbunus@gmail.com>
2017-12-02 11:00:03 -0400

    correção de bug

--- JS/JS_relacionamento_git.js
+++ JS/JS_relacionamento_git.js
@@ -4,7 +4,7 @@
 function buscar_repositorios(){
   var linguagem = $('#select_linguagem option:selected').val();
   $.ajax({
-        url: "https://api.github.com/search/repositories?q=language:"+ linguagem +"&sort=star",
+        url: "https://api.github.com/search/repositories?q=language:"+ linguagem +"&sort=stars",
         type: 'GET',
         dataType: 'json',
         //async: 'false',
@@ -17,7 +17,7 @@ function buscar_repositorios(){
             var itens = data['items'];
             var html_party = '<table class="tab_lista">';
             for(var i = 0; i < itens.length; i++){
-              html_party += '<tr class="linha_lista" onclick="destacar(this), escreve_arg_detalhe_repositorio(' + itens[i].id + ', ' + itens[i].commits + ', ' + itens[i].contributors + ', \'' + itens[i].name + '\', \'' + itens[i].language + '\', \'' + itens[i].description + '\', \'' + itens[i].url + '\', \'' + itens[i].created_at + '\', ' + itens[i].forks + '), escreve_arg_grava_repositorio('  + itens[i].id + ', ' + itens[i].commits + ', ' + itens[i].contributors + ', \'' + itens[i].name + '\', \'' + itens[i].language + '\', \'' + itens[i].description + '\', \'' + itens[i].html_url + '\', \'' + itens[i].url + '\', \'' + itens[i].created_at + '\', ' + itens[i].forks + ')"><td style="width:250px;">' + itens[i].name + '</td><td style="width:350px;">' + se_nulo(itens[i].description) + '</td><td style="width:180px;">' + itens[i].created_at + '</td><td style="width:80px;"><a href=\"' + itens[i].html_url + '\" target="_blank">Acessar</a></td></tr>';
+              html_party += '<tr class="linha_lista" onclick="destacar(this), escreve_arg_detalhe_repositorio(' + itens[i].id + ', \'' + itens[i].name + '\', \'' + itens[i].language + '\', \'' + itens[i].description + '\', \'' + itens[i].url + '\', \'' + itens[i].created_at + '\', ' + itens[i].forks + '), escreve_arg_grava_repositorio('  + itens[i].id + ',  \'' + itens[i].name + '\', \'' + itens[i].language + '\', \'' + itens[i].description + '\', \'' + itens[i].html_url + '\', \'' + itens[i].url + '\', \'' + itens[i].created_at + '\', ' + itens[i].forks + ')"><td style="width:250px;">' + itens[i].name + '</td><td style="width:350px;">' + se_nulo(itens[i].description) + '</td><td style="width:180px;">' + itens[i].created_at + '</td><td style="width:80px;"><a href=\"' + itens[i].html_url + '\" target="_blank">Acessar</a></td></tr>';
             };
             html_party += '</table>';
             $('#repositorios_git').html(html_party);
--- JS/controle_de_funcoes.js
+++ JS/controle_de_funcoes.js
@@ -3,13 +3,13 @@ EXECUTA ALTERAÇÕES SOBRE DIVERSAS FUNÇÕES COMO ALTERAÇÃO DE ARGUMENTOS E C
 #####################################################################################*/
 
 //altera argumento da função "grava_repositorio()", setando qual repositorio deve ser gravado
-function escreve_arg_grava_repositorio(id, commits, contributors, name, language, description, html_url, url, created_at, forks){
-  $('#bt_grava_repositorio').attr('onclick','grava_repositorio(' + id + ', ' + commits + ', ' + contributors + ', \'' + name + '\',\'' + language + '\', \'' + description + '\', \'' + html_url + '\', \'' + url + '\', \'' + created_at + '\', ' + forks + ')');
+function escreve_arg_grava_repositorio(id, name, language, description, html_url, url, created_at, forks){
+  $('#bt_grava_repositorio').attr('onclick','grava_repositorio(' + id + ', \'' + name + '\',\'' + language + '\', \'' + description + '\', \'' + html_url + '\', \'' + url + '\', \'' + created_at + '\', ' + forks + ')');
 }
 
 //altera argumento da função "detalhe_repositorio()", setando qual repositorio deve ser visualizado
-function escreve_arg_detalhe_repositorio(id, commits, contributors, name, language, description, html_url, url, created_at, forks){
-  $('#bt_detalhe_repositorio').attr('onclick','detalhe_repositorio('+ id + ',' + commits + ',' + contributors + ', \'' + name + '\', \'' + language + '\', \'' + description + '\', \'' + url + '\', \'' + created_at + '\', ' + forks + ')');
+function escreve_arg_detalhe_repositorio(id, name, language, description, html_url, url, created_at, forks){
+  $('#bt_detalhe_repositorio').attr('onclick','detalhe_repositorio(' + id + ', \'' + name + '\', \'' + language + '\', \'' + description + '\', \'' + url + '\', \'' + created_at + '\', ' + forks + ')');
 }
 
 //altera argumento da função "apaga_repositorio_armazenado()", setando qual repositorio deve ser apagado
