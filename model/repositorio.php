a50dd99c0a236b8a58262d5b5b05ddcc7fefc22d
Eduardo Garbin <garbunus@gmail.com>
2017-12-02 15:49:22 -0400

    alterando nome de funções

--- model/repositorio.php
+++ model/repositorio.php
@@ -5,28 +5,28 @@ require_once("DAO/DAO_repositorio.php");
 class repositorio{
 
   //########################### Grava dados no banco ###########################
-    public function grava_repositorio($id, $nome, $linguagem, $descricao, $html_url, $url, $criado_em, $forks){
+    public function gravar_repositorio($id, $nome, $linguagem, $descricao, $html_url, $url, $criado_em, $forks){
       $dao = new DAO_repositorio;
       $retorno = $dao->DAO_grava_repositorio($id, $nome, $linguagem, $descricao, $html_url, $url, $criado_em, $forks);
       return $retorno;
     }
 
   //########################### Busca dados no banco ###########################
-    public function busca_repositorios(){
+    public function buscar_repositorios(){
       $dao = new DAO_repositorio;
       $retorno = $dao->DAO_busca_repositorios();
       return $retorno;
     }
 
   //######### Busca detalhes de repositório selecionado dados no banco #########
-    public function busca_detalhes_repositorio($id){
+    public function buscar_detalhes_repositorio($id){
       $dao = new DAO_repositorio;
       $retorno = $dao->DAO_busca_detalhes_repositorio($id);
       return $retorno;
     }
 
   //################## Apaga repositório selecionado do banco ##################
-    public function apaga_repositorio($id){
+    public function apagar_repositorio($id){
       $dao = new DAO_repositorio;
       $retorno = $dao->DAO_apaga_repositorio($id);
       return $retorno;
