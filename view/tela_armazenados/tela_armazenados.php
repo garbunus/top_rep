6cc29bbfc047499782ddc01d17bca4a9646b8219
Eduardo Garbin <garbunus@gmail.com>
2017-12-03 16:01:59 -0400

    update tela de repositórios armazenados

--- view/tela_armazenados/tela_armazenados.php
+++ view/tela_armazenados/tela_armazenados.php
@@ -1,6 +1,12 @@
 
 <div id="tela_armazenados" class="telas">
-
+  <div id="cabecalho_repositorios_armazenados">
+    <table>
+      <tr>
+        <td style="width:240px;">Nome:</td><td style="width:340px;">Descrição:</td><td style="width:180px;">Linguagem:</td><td style="width:80px;"></td>
+      </tr>
+    </table>
+  </div>
   <div id="repositorios_armazenados" class="bloco_interno"></div>
   <div id="barra_botoes_armazenados" class="bloco_interno">
     <input id="bt_apaga_repositorio_armazenado" type="button" class="bt_geral" value="Apagar" onclick="apaga_repositorio_armazenado()">
