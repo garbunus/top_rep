620a20ab624a65bf9366d6c80644560df35018de
Eduardo Garbin <garbunus@gmail.com>
2017-12-04 17:58:53 -0400

    alteração de nome da aplicação

--- view/main.php
+++ view/main.php
@@ -3,7 +3,7 @@
 <HTML>
   <HEAD>
     <meta charset="utf8">
-    <title>TOP{GIT}</title>
+    <title>TOP{repo}</title>
 
     <!-- ADD JQUERY -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
