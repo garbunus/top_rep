69f93c80758ae27194615ef4ce3f149d91be5ed6
Eduardo Garbin <garbunus@gmail.com>
2017-12-04 23:17:39 -0400

    redirecionando

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+                echo "<meta HTTP-EQUIV = 'Refresh' CONTENT='0; URL=view/main.php'>";
+
+        ?>
+    </body>
+</html>
